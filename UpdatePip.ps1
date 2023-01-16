c3f513627ea32df19e031edaf39876ae1962c04e
Green <Green>
2023-01-15 22:20:31 -0500

    UpdatePip
    Automatically Update Pip

---
+++ UpdatePip.ps1
@@ -0,0 +1,9 @@
+﻿#! powershell.exe
+# Update pip
+$pip = 'pip'
+$version = (Invoke-Expression "$pip --version").Trim()
+Write-Host "Updating $pip version $version..."
+Invoke-Expression "$pip install --upgrade pip"
+
+# Keep the PowerShell window open
+Read-Host -Prompt "Press Enter to close the window..."
