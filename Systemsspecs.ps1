556b97cc67eb97b7b6884bd9abeb5b96a80314e5
Green <Green>
2023-01-15 22:48:09 -0500

    Create Systemsspecs.ps1
    Easy way to quickly check windows specs in cmd rather than system info - though that's probably just as fast and or more detailed.

---
+++ Systemsspecs.ps1
@@ -0,0 +1,15 @@
+﻿$computer = gwmi win32_computersystem
+$cpu = gwmi win32_processor
+$memory = gwmi win32_physicalmemory
+$os = gwmi win32_operatingsystem
+
+$totalMem = ($memory | ForEach-Object {[Math]::Round($_.Capacity/1GB,2)} | Measure-Object -sum).sum
+
+Write-Host "Computer Name: " $computer.Name
+Write-Host "Manufacturer: " $computer.Manufacturer
+Write-Host "Model: " $computer.Model
+Write-Host "Number of Processors: " $computer.NumberOfProcessors
+Write-Host "Processor: " $cpu.Name
+Write-Host "Total Memory: " $totalMem "GB"
+Write-Host "Operating System: " $os.Caption
+Read-Host -Prompt "Press Enter to exit"
